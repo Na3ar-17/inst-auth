edb22f1c77cfb0dc77c168bb65671b3132e20729
Na3ar-17 <gavruk2017@gmail.com>
2024-02-15 18:57:18 +0200

    wrote all html

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+// <section>
+// <header class="header">INSTAGRAM</header>
+// <main class="main">
+//   <div class="user">
+//     <div class="auth">
+//       <div class="inputs">
+//         <input type="text" class="input" placeholder="Номер
+//         телефону,і'мя користувача або ел. ...>
+//         <input type="text" class="input" placeholder="Пароль" />
+//       </div>
+//     </div>
+//   </div>
+// </main>
+// </section>
+// `
